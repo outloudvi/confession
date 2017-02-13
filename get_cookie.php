44c7ad6b26951436b51b7b37c6d9fa1625f070e6
Outloudvi <outloudvi@outlook.com>
2017-02-14 01:40:55 +0800

    sth about MySQL

--- get_cookie.php
+++ get_cookie.php
@@ -12,10 +12,6 @@
 <a href="index.php">&lt;&lt;Main page</a> | <a href="submit.php">Confess!</a> | <a href="login.php">Login</a> if you have already got a cookie
 <hr />
 
-<?php
-    if
-?>
-
 <form method="post" action="give_cookie.php">
     <input type="submit" name="submit" value="Get you cookie!">
 </form>
